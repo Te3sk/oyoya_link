9c2fe7aa0c3dc7a92d0e2020d22d1882f8801c4e
Niccolò Fulgaro <nicco.fulgaro@outlook.it>
2024-04-10 19:50:00 +0200

    doing link box, find a way to make all the box clickable

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,20 +1,26 @@
 import React from "react";
 
-function Box(title, link, img) {
+function handleClick () {
+
+}
+
+function Box({ title, link, img }) {
   return (
-  <div className=" w-5/6 bg-red-400 h-24 mt-3">
-    <div>
-        
-    </div>
-  </div>
-  )
+      <div className="border-2 border-red-400 h-20 w-11/12 m-3 flex flex-row items-center">
+        <div className="w-16 h-16 ml-2 bg-lime-400">
+          {/* <img src={} alt="playlist image"/> */}
+        </div>
+        <div className="m-5 text-lg font-bold text-white">{title}</div>
+      </div>
+  );
 }
 
 export default function MainBody() {
   return (
-    <div className="bg-black h-screen border-t-2 border-white">
-      <div className="h-dvh  flex flex-col items-center border-2 border-amber-500">
-        <Box />
+    <div className="bg-black border-t-2 border-white">
+      <div className="h-dvh  flex flex-col items-center border-2 overflow-scroll max-h-svh border-amber-500">
+        <Box title="Oyoya Party: Flipper & Friends" />
+        <Box title="Oyoya x Exploit: End Fossil" />
       </div>
     </div>
   );

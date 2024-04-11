b7f8a4f8dd104c50b425daf4ade5f26dbb7d5ce6
Niccolò Fulgaro <nicco.fulgaro@outlook.it>
2024-04-11 12:08:54 +0200

    commit to try redirecting

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,12 +1,14 @@
 import React from "react";
-
-function handleClick () {
-
-}
+import { Link } from "react-router-dom"
 
 function Box({ title, link, img }) {
+  const handleClick = () => {
+    window.open(link)
+    // window.open(link);
+  }
+
   return (
-      <div className="border-2 border-red-400 h-20 w-11/12 m-3 flex flex-row items-center">
+      <div className="border-2 border-red-400 h-20 w-11/12 m-3 flex flex-row items-center" onClick={handleClick}>
         <div className="w-16 h-16 ml-2 bg-lime-400">
           {/* <img src={} alt="playlist image"/> */}
         </div>
@@ -20,7 +22,7 @@ export default function MainBody() {
     <div className="bg-black border-t-2 border-white">
       <div className="h-dvh  flex flex-col items-center border-2 overflow-scroll max-h-svh border-amber-500">
         <Box title="Oyoya Party: Flipper & Friends" />
-        <Box title="Oyoya x Exploit: End Fossil" />
+        <Box title="Oyoya x Exploit: End Fossil" link="https://open.spotify.com/playlist/1t0X3GBOBCL3TV0vwn0gCo?si=257416284b6f4867"/>
       </div>
     </div>
   );
